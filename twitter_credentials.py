944418e4f11d3ebedf0a685752ce02a126d046f6
Thomas Weinandy <tomweinandy@gmail.com>
2019-11-11 10:47:11 -0500

    Add files via upload

---
+++ twitter_credentials.py
@@ -0,0 +1,7 @@
+
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = 'EXAMPLE'
+ACCESS_TOKEN_SECRET = 'EXAMPLE'
+CONSUMER_KEY = 'EXAMPLE'
+CONSUMER_SECRET = 'EXAMPLE'
